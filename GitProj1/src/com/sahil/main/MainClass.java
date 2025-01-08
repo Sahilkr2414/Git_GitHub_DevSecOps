bbb777b3f39e16522b1b643cdb16e0c4612f8455
Lenovo <Lenovo@192.168.10.35>
2025-01-08 21:04:27 +0530

    Developer had added sub method and main method

---
+++ GitProj1/src/com/sahil/main/MainClass.java
@@ -0,0 +1,14 @@
+package com.sahil.main;
+
+import com.sahil.service.Calculator;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Calculator.add(10, 20));
+		System.out.println(Calculator.sub(70, 20));
+
+	}
+
+}
